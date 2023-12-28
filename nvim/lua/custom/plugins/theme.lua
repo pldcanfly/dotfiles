461501df48a72c8e1dbc17c6095d65896d75d3a1
Thomas Striegl <pldcanfly@gmail.com>
2023-12-29 00:58:54 +0100

    Alacritty Theme, Fixed nvim colors, removed openRGB logs

--- nvim/lua/custom/plugins/theme.lua
+++ nvim/lua/custom/plugins/theme.lua
@@ -1,8 +1,24 @@
+-- return {
+--   'catppuccin/nvim',
+--   name = 'catppuccin',
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme 'catppuccin-mocha'
+--   end,
+-- }
+
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  'EdenEast/nightfox.nvim',
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'catppuccin'
-  end,
+    -- require('nightfox').setup({
+    --   options = {
+    --     terminal_colors = false,
+    --     styles = {
+    --       comments = "italic",
+    --     }
+    --   }
+    -- })
+    vim.cmd.colorscheme 'terafox'
+  end
 }
