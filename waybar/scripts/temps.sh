24c199c8eabede347850b4abd2eebd68c58f135e
pldcanfly <pldcanfly@gmail.com>
2024-04-11 10:21:45 +0200

    waybar theme

--- waybar/scripts/temps.sh
+++ waybar/scripts/temps.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-
-cpu_temp=$(sensors -u | grep "k10temp" -A 3 | grep "temp1_input" | awk '{printf "%.0f", $2}')
-gpu_temp=$(sensors -u | grep "amdgpu-pci-0300" -A 9 | grep "temp1_input:" | awk '{printf "%.0f", $2}')
-
-echo '{"text": "   '$cpu_temp'°  󰍹   '$gpu_temp'°", "class": "temps"}'
+/nix/store/kgkva5aga5shqyzam12aald9ijakaj6m-home-manager-files/.config/waybar/scripts/temps.sh
