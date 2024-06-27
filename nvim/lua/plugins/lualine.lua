bdffba7cdd6ae1d86fef41f8a3f6ec20d9c259d0
pldcanfly <pldcanfly@gmail.com>
2024-06-27 12:30:19 +0200

    nvim: Setup Dap

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,13 +1,16 @@
 return {
-  -- Set lualine as statusline
   'nvim-lualine/lualine.nvim',
-  -- See `:help lualine.txt`
   opts = {
     options = {
       icons_enabled = true,
       theme = 'terafox',
       component_separators = '|',
       section_separators = '',
+      ignore_focus = {
+      "dapui_watches", "dapui_breakpoints",
+      "dapui_scopes", "dapui_console",
+      "dapui_stacks", "dap-repl"
+    }
     },
   },
 }
