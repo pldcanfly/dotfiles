5b703acca7b36412f831f10b4f6d885fcd76d86a
Thomas Striegl <pldcanfly@gmail.com>
2024-08-20 16:57:29 +0200

    nvim: QoL

--- nvim/lua/plugins/nonels.lua
+++ nvim/lua/plugins/nonels.lua
@@ -6,7 +6,10 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.phpcsfixer,
-        null_ls.builtins.formatting.prettierd
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.sqlfluff.with({
+          extra_args = { "--dialect", "postgres" }, -- change to your dialect
+        }),
       }
     })
   end,
