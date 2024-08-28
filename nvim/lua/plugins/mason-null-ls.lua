d86d5cba2709cb273bb675dcba65749a846cbae8
pldcanfly <pldcanfly@gmail.com>
2024-08-28 21:30:57 +0200

    wip: lsp/formatter

---
+++ nvim/lua/plugins/mason-null-ls.lua
@@ -0,0 +1,11 @@
+return {
+	"jay-babu/mason-null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvimtools/none-ls.nvim",
+	},
+	opts = {
+		ensure_installed = require("config.langs").nullls.ensure_installed,
+	},
+}
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,10 +1,4 @@
 return {
 	"williamboman/mason.nvim",
-	name = "mason.nvim",
-	cmd = "Mason",
-	dependencies = {
-		{ "williamboman/mason-lspconfig.nvim", opts = { automatic_installation = true, ensure_installed = require("config.langs").ensure_lsp } }
-	},
-	build = ":MasonUpdate",
 	config = true
 }
