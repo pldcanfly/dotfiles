4aeeed8e0e4d07618a24931675587c988c1b14d7
pldcanfly <pldcanfly@gmail.com>
2024-08-30 23:35:32 +0200

    wip: ts-tools not formating

--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -5,6 +5,10 @@ return {
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 		require("typescript-tools").setup({
 			capabilities = capabilities,
+			on_attach = function(client)
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentRangeFormattingProvider = false
+			end,
 		})
 	end,
 }
