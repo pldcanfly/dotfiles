d3afe9de7de7c1016899c804b02989528b8bd500
pldcanfly <pldcanfly@gmail.com>
2025-07-31 00:57:05 +0200

    themechanger: unified some things

--- _themechanger/internal/builders/niri.go
+++ _themechanger/internal/builders/niri.go
@@ -8,6 +8,21 @@ import (
 	"text/template"
 )
 
+func parse(path string, vars map[string]any) (string, error) {
+	t, err := template.ParseFiles(path)
+	if err != nil {
+		return "", fmt.Errorf("parsing: %w", err)
+	}
+
+	// That pattern is way to repated... sort that out and make it a loop
+
+	var buff bytes.Buffer
+	if err = t.Execute(&buff, vars); err != nil {
+		return "", fmt.Errorf("executing: %w", err)
+	}
+	return buff.String(), nil
+}
+
 func BuildNiri(repo string, computer string, theme string) error {
 	base, err := template.ParseFiles(filepath.Join(repo, "niri/base.text.tmpl"))
 	if err != nil {
@@ -16,68 +31,36 @@ func BuildNiri(repo string, computer string, theme string) error {
 
 	vars := make(map[string]any)
 
-	as, err := template.ParseFiles(filepath.Join(repo, fmt.Sprintf("niri/autostart/%s.text.tmpl", computer)))
+	vars["Autostart"], err = parse(filepath.Join(repo, fmt.Sprintf("niri/autostart/%s.text.tmpl", computer)), vars)
 	if err != nil {
-		return fmt.Errorf("parsing autostart: %w", err)
+		return fmt.Errorf("autostart: %w", err)
 	}
 
-	var asbuff bytes.Buffer
-	if err = as.Execute(&asbuff, vars); err != nil {
-		return fmt.Errorf("executing autostart: %w", err)
-	}
-	vars["Autostart"] = asbuff.String()
-
-	o, err := template.ParseFiles(filepath.Join(repo, fmt.Sprintf("niri/outputs/%s.text.tmpl", computer)))
+	vars["Outputs"], err = parse(filepath.Join(repo, fmt.Sprintf("niri/outputs/%s.text.tmpl", computer)), vars)
 	if err != nil {
-		return fmt.Errorf("parsing outputs: %w", err)
+		return fmt.Errorf("outputs: %w", err)
 	}
 
-	var obuff bytes.Buffer
-	if err = o.Execute(&obuff, vars); err != nil {
-		return fmt.Errorf("executing outputs: %w", err)
-	}
-	vars["Outputs"] = obuff.String()
-
-	i, err := template.ParseFiles(filepath.Join(repo, "niri/input.text.tmpl"))
+	vars["Input"], err = parse(filepath.Join(repo, "niri/input.text.tmpl"), vars)
 	if err != nil {
-		return fmt.Errorf("parsing input: %w", err)
+		return fmt.Errorf("input: %w", err)
 	}
-	var ibuff bytes.Buffer
-	if err = i.Execute(&ibuff, vars); err != nil {
-		return fmt.Errorf("executing input: %w", err)
-	}
-	vars["Input"] = ibuff.String()
 
-	b, err := template.ParseFiles(filepath.Join(repo, "niri/binds.text.tmpl"))
+	vars["Binds"], err = parse(filepath.Join(repo, "niri/binds.text.tmpl"), vars)
 	if err != nil {
-		return fmt.Errorf("parsing binds: %w", err)
+		return fmt.Errorf("binds: %w", err)
 	}
-	var bbuff bytes.Buffer
-	if err = b.Execute(&bbuff, vars); err != nil {
-		return fmt.Errorf("executing binds: %w", err)
-	}
-	vars["Binds"] = bbuff.String()
 
 	// This needs theming!!
-	s, err := template.ParseFiles(filepath.Join(repo, "niri/style.text.tmpl"))
+	vars["Style"], err = parse(filepath.Join(repo, "niri/style.text.tmpl"), vars)
 	if err != nil {
-		return fmt.Errorf("parsing style: %w", err)
+		return fmt.Errorf("style: %w", err)
 	}
-	var sbuff bytes.Buffer
-	if err = s.Execute(&sbuff, vars); err != nil {
-		return fmt.Errorf("executing style: %w", err)
-	}
-	vars["Style"] = sbuff.String()
 
-	wr, err := template.ParseFiles(filepath.Join(repo, "niri/windowrules.text.tmpl"))
+	vars["Windowrules"], err = parse(filepath.Join(repo, "niri/windowrules.text.tmpl"), vars)
 	if err != nil {
-		return fmt.Errorf("parsing windowrules: %w", err)
+		return fmt.Errorf("windowrules: %w", err)
 	}
-	var wrbuff bytes.Buffer
-	if err = wr.Execute(&obuff, vars); err != nil {
-		return fmt.Errorf("executing windowrules: %w", err)
-	}
-	vars["Windowrules"] = wrbuff.String()
 
 	// This can just fail .. not a biggie
 	os.Remove(filepath.Join(repo, "niri/config.kdl"))
