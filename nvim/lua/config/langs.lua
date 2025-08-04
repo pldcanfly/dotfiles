a24ae418f9cbeb3c9fafc9f6d95336d21d7d7925
pldcanfly <pldcanfly@gmail.com>
2025-08-04 16:41:50 +0200

    changes?

--- nvim/lua/config/langs.lua
+++ nvim/lua/config/langs.lua
@@ -105,36 +105,34 @@ return {
 						on_init = function(client)
 							local path = client.workspace_folders[1].name
 							if
-								vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc")
+							    vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc")
 							then
 								return
 							end
 
-							client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-								runtime = {
-									-- Tell the language server which version of Lua you're using
-									-- (most likely LuaJIT in the case of Neovim)
-									version = "LuaJIT",
-								},
-								-- Make the server aware of Neovim runtime files
-								workspace = {
-									checkThirdParty = false,
-									library = {
-										vim.env.VIMRUNTIME,
-										-- Depending on the usage, you might want to add additional paths here.
-										-- "${3rd}/luv/library"
-										-- "${3rd}/busted/library",
+							client.config.settings.Lua = vim.tbl_deep_extend("force",
+								client.config.settings.Lua, {
+									runtime = {
+										-- Tell the language server which version of Lua you're using
+										-- (most likely LuaJIT in the case of Neovim)
+										version = "LuaJIT",
 									},
-									-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-									-- library = vim.api.nvim_get_runtime_file("", true)
-								},
-							})
+									-- Make the server aware of Neovim runtime files
+									workspace = {
+										checkThirdParty = false,
+										library = {
+											vim.env.VIMRUNTIME,
+											-- Depending on the usage, you might want to add additional paths here.
+											-- "${3rd}/luv/library"
+											-- "${3rd}/busted/library",
+										},
+										-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+										-- library = vim.api.nvim_get_runtime_file("", true)
+									},
+								})
 						end,
 					})
 				end,
-				-- ts_ls = function()
-				-- 	-- I am handled by typescript-tools
-				-- end,
 			}
 
 			for _, server in ipairs(installed) do
