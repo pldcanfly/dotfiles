bb32051d010eb81c50fc7a09a77e6eb0750c66fd
pldcanfly <pldcanfly@gmail.com>
2025-09-23 09:58:27 +0200

    move to alpha

--- misc/nvim/lua/plugins/mini.lua
+++ misc/nvim/lua/plugins/mini.lua
@@ -2,7 +2,6 @@ return {
 	{ "nvim-mini/mini.statusline", version = "*", config = true },
 	{ "nvim-mini/mini.splitjoin", version = "*", config = true },
 	{ "nvim-mini/mini.move", version = "*", config = true },
-	{ "nvim-mini/mini.starter", version = "*", config = true },
 	{
 		"nvim-mini/mini.cursorword",
 		version = "*",
