b6240d69a36bb2e6d4639161bc68011b749ac336
pldcanfly <pldcanfly@gmail.com>
2026-08-30 15:42:54 +0200

    deprecated

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,9 +1,9 @@
 -- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
+-- See `:help vim.hl.hl_op()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.hl_op()
 	end,
 	group = highlight_group,
 	pattern = "*",
